2d082eb07b65a95d5e19cfa4d6e4c9f104494c0d
forbesrj <forbesrj@gmail.com>
2014-08-29 20:47:36 -0600

    About page added

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,9 @@
 /**
  * Created by Ryan on 8/29/2014.
  */
+function HeaderController($scope, $location)
+{
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+}
--- js/routing.js
+++ js/routing.js
@@ -1,3 +1,10 @@
 /**
  * Created by Ryan on 8/29/2014.
  */
+var app = angular.module('cfwModule', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/about', {templateUrl:'partials/about.html'})
+        .otherwise({redirectTo: '/', templateUrl:'partials/home.html'})
+}]);
