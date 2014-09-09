be60e03914859232477ac258b95e80c1585f102e
forbesrj <forbesrj@gmail.com>
2014-09-09 11:32:57 -0600

    Renamed and added files to git. Agenda renamed to vision.

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,17 @@
 /**
  * Created by Ryan on 8/29/2014.
  */
-function HeaderController($scope, $location)
+function HeaderController($scope, $location, $anchorScroll)
 {
     $scope.isActive = function (viewLocation) {
         return viewLocation === $location.path();
+    };
+}
+
+function AboutController($scope, $location, $anchorScroll){
+
+    $scope.goToTop = function() {
+        $location.hash('top');
+        $anchorScroll();
     };
 }
