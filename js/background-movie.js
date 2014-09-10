89909b475cb3c7d42bd73460f4755d469a6e3ee6
forbesrj <forbesrj@gmail.com>
2014-09-10 14:12:36 -0600

    .content.social top fix

--- js/background-movie.js
+++ js/background-movie.js
@@ -1,7 +1,7 @@
 
 $(function() {
     var isTouch = Modernizr.touch;
-    var BV = new $.BigVideo({forceAutoplay:isTouch});
+    var BV = new $.BigVideo({forceAutoplay:isTouch, useFlashForFirefox:false});
     BV.init();
     BV.show('media/vid/Colorado Fashion Week-HD.mp4', {ambient:false, doLoop:true});
     BV.getPlayer().volume(0);
